b2a6056773abb8c6754e951c790f64bffa881bba
aniket0131 <aniketchapla@gmail.com>
2024-06-09 17:02:18 +0530

    axios changes

--- server/index.js
+++ server/index.js
@@ -2,27 +2,27 @@ const express = require('express');
 const database = require('./config/config');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const { register } = require('./Controllers/authController');
 const userRoutes = require("./Routes/AuthRoutes");
 const createjob = require("./Routes/jobRoutes");
 const CandidateRoutes = require("./Routes/CandidateRoutes");
 const cookieParser = require('cookie-parser'); // Add this line
-// const cors = require('cors')    
+const cors = require('cors')    
 dotenv.config();
 
-// app.use(cors(
-//     {
-//         origin: ['https://deploy-mern-'],
-//         methods: ["POST","GET"],    
-//         credentials: true,
-//     }
-// ))
+app.use(cors(
+    {
+        origin: ['https://codsoft2-xws7-3i122judz-anikets-projects-165fe49e.vercel.app/'],
+        methods: ["POST","GET"],    
+        credentials: true,
+    }
+))
 const app = express();
 const PORT = 4000;
 
 const corsOptions = {
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: 'https://codsoft2-xws7-3i122judz-anikets-projects-165fe49e.vercel.app/', // Allow requests from this origin
     credentials: true, // Allow cookies and other credentials
   };
 // Init Middleware
